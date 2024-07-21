328b5397c6ff9420bd1b7e8d992c437997943f38
Servolo P. <138056513+srlv@users.noreply.github.com>
2024-07-21 23:56:26 +0000

    Resolução

---
+++ Questão4.java
@@ -0,0 +1,18 @@
+package grauscentigrados;
+import java.util.Scanner;
+public class GrausCentigrados {
+
+      public static void main(String[] args) {
+        float celsius, fahrenheit;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Informe a temperatura em Celsius: ");
+        celsius = teclado.nextFloat();
+
+        fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+
+        teclado.close();
+    }
+}
