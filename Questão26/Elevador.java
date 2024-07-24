9a75e06fbd7f327f2221c9eb4ddf23b03e5404c4
Servolo P. <138056513+srlv@users.noreply.github.com>
2024-07-24 17:30:53 -0300

    Resolução

--- Questão26/Elevador.java
+++ Questão26/Elevador.java
@@ -1,17 +1,3 @@
-/* Crie uma classe denominada Elevador para armazenar as informações de um elevador
-dentro de um prédio. A classe deve armazenar o número do andar atual, o total de
-andares no prédio, a capacidade do elevador e quantas pessoas estão presentes nele.
-Outras classes não devem ter acesso direto aos atributos de Elevador. A classe deve
-também disponibilizar os seguintes métodos:
-• construtor : que deve receber como parâmetros a capacidade do elevador e o total
-de andares no prédio (um elevador sempre começa no térreo e vazio);
-• entra : para acrescentar uma pessoa no elevador (só deve acrescentar se ainda
-houver espaço);
-• sai : para remover uma pessoa do elevador (só deve remover se houver alguém
-dentro dele);
-• sobe : para subir um andar (não deve subir se já estiver no último andar);
-• desce : para descer um andar (não deve descer se já estiver no térreo).*/
-
 package demoelevador;
 
 public class Elevador {
@@ -61,8 +47,8 @@ public class Elevador {
             System.out.println(" descendo andar " + numAndarAtual);
 
         }else{
-            System.out.println(" voce esta ja esta no terreo ");
+            System.out.println(" voce ta querendo chegar no inferno ehh ");
         }
     }
 }
-  
+    
